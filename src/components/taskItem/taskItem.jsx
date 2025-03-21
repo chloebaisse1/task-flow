e8d2d869f56971f1fc5a3fc6cf4511f1385f1805
chloebaisse1 <baissechloe@gmail.com>
2025-03-21 16:59:45 +0400

    afficher un message en fonction du nbrs de tâches

--- src/components/taskItem/taskItem.jsx
+++ src/components/taskItem/taskItem.jsx
@@ -4,12 +4,26 @@ import styles from "./TaskItem.module.css"
 export const TaskItem = ({ task, editTask, deleteTask }) => {
   return (
     <li
-      className={`${styles.container} ${styles.default}`}
+      className={`${styles.container} ${
+        task?.completed ? styles.success : styles.default
+      }`}
       onClick={() => editTask(task.id, !task.completed)}
     >
       <div className={styles.item}>
-        <div className={`${styles.id} ${styles.idDefault}`}>{task.id}</div>
-        <div className={styles.contentDefault}>{task.title}</div>
+        <div
+          className={`${styles.id} ${
+            task?.completed ? styles.idSuccess : styles.idDefault
+          }`}
+        >
+          {task.id}
+        </div>
+        <div
+          className={
+            task?.completed ? styles.contentSuccess : styles.contentDefault
+          }
+        >
+          {task.title}
+        </div>
       </div>
       <button className="button-primary">x</button>
     </li>
--- src/components/taskList/taskList.jsx
+++ src/components/taskList/taskList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 // Ce composant est utilisé pour afficher la liste des tâches.
 import { TaskItem } from "../taskItem/taskItem"
 import styles from "./TaskList.module.css"
@@ -16,14 +17,34 @@ export const TaskList = ({
     />
   ))
 
+  if (taskList && taskList.length > 0) {
+    return (
+      <div className="box">
+        <h2 className={styles.title}>
+          {incompletedTasks > 0 && (
+            <>
+              🗒️ Il te reste encore{" "}
+              <span className="important">{incompletedTasks}</span>
+              tâches à accomplir !
+            </>
+          )}
+
+          {incompletedTasks === 0 && (
+            <> 🤝 Génial, tu as accompli toutes tes tâches !</>
+          )}
+        </h2>
+
+        {tasksList && tasksList.length > 0 && (
+          <ul className={styles.container}>{taskList}</ul>
+        )}
+      </div>
+    )
+  }
   return (
     <div className="box">
-      <h2 className={styles.title}>
-        🗒️ Il te reste encore {incompletedTasks} tâches à accomplir !
+      <h2 className={styles.emptyState}>
+        👋 Salut, tu n'as rien à faire ! Profite de ton temps libre !
       </h2>
-      {tasksList && tasksList.length > 0 && (
-        <ul className={styles.container}>{taskList}</ul>
-      )}
     </div>
   )
 }
